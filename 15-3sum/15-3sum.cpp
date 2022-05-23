0924c8654d986e100dba22d8455a50addb4f2078
Anshuman Sharma <code4anshu@gmail.com>
2022-05-23 20:56:44 +0530

    Time: 97 ms (73.31%), Space: 21.2 MB (40.25%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -5,13 +5,15 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
         for(int i = 0; i<n-2; i++){
+            if(nums[i]>0) break;
            int target = (-1)*nums[i];
            int l = i+1, h = n-1;
+            if(nums[i]+nums[l]>0) break;
            while(l<h){
                if(nums[l] + nums[h] == target){
                    vector<int> xy(3);
                    xy[0] = nums[i], xy[1] = nums[l], xy[2] = nums[h];
-                   ans.push_back(xy);
+                   ans.emplace_back(xy);
                    while(l<h && nums[l]==nums[l+1]) l++;
                    while(h>l && nums[h]==nums[h-1]) h--;
                    l++, h--;
