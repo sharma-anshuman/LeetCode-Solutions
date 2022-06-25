f149c49ce2ceb6fdcd714a245d280e61a0101f5d
Anshuman Sharma <code4anshu@gmail.com>
2022-06-25 17:06:31 +0530

    Time: 51 ms (24.65%), Space: 27.1 MB (53.59%) - LeetHub

---
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int ct=0;
+        for(int i = 1; i<nums.size(); i++){
+            if(nums[i]<nums[i-1]){
+                ct++;
+                if(ct>1) return false;
+                if(i == 1 || nums[i-2]<=nums[i]){
+                    nums[i-1] = nums[i];
+                }
+                else{
+                    nums[i] = nums[i-1];
+                }
+            }
+        }
+        return true;
+    }
+};
+
+
+//1 2 3 10 5 6 7
