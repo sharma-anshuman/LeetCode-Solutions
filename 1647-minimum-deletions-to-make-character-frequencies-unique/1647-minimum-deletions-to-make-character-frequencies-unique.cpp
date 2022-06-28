a4b8bb1cc349697b800a9613f5f8abcb83f70977
Anshuman Sharma <code4anshu@gmail.com>
2022-06-28 11:30:19 +0530

    Time: 97 ms (47.48%), Space: 17.6 MB (14.17%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        int arr[26];
+        int n = s.size();
+        memset(arr, 0, sizeof(arr));
+        for(int i = 0; i<n; i++){
+            arr[s[i] - 'a']++;
+        }
+        int ct[n+1];
+        memset(ct, 0, sizeof(ct));
+        for(int i = 0; i<26; i++){
+            ct[arr[i]]++;
+        }
+        int ans = 0;
+        bool flag = false;
+        for(int i = 0; i<26; i++){
+            while(ct[arr[i]] > 1){
+                //cout<<i<<' '<<arr[i]<<' '<<ct[arr[i]]<<endl;
+                flag = false;
+                int x = arr[i];
+                int p = 0;
+                for(int k = x-1; k>=0; k--){
+                    p++;
+                    if(!ct[k]){
+                        //cout<<k<<' '<<p<<endl;
+                        ct[arr[i]]--;
+                        arr[i] = k;
+                        ct[k] = 1;
+                        flag = true;
+                        ans+=p;
+                        break;
+                    }
+                }
+                if(!flag){
+                  ans+=p;  
+                  ct[arr[i]]--;
+                } 
+                
+            }
+        }
+        return ans;
+    }
+};
