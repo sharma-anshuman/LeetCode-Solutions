6c1186406ec56376d757b4f1dbda097d21bf958d
Anshuman Sharma <code4anshu@gmail.com>
2022-06-28 14:14:16 +0530

    Time: 106 ms (40.57%), Space: 17.4 MB (58.78%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,40 +1,21 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        int arr[26];
-        int n = s.size();
-        memset(arr, 0, sizeof(arr));
-        for(int i = 0; i<n; i++){
-            arr[s[i] - 'a']++;
-        }
-        int ct[n+1];
-        memset(ct, 0, sizeof(ct));
-        for(int i = 0; i<26; i++){
-            ct[arr[i]]++;
+       unordered_map<char, int> arr;
+        for(int i = 0; i<s.size(); i++){
+            arr[s[i]]++;
         }
         int ans = 0;
-        bool flag = false;
-        for(int i = 0; i<26; i++){
-            while(ct[arr[i]] > 1){
-                flag = false;
-                int x = arr[i];
-                int p = 0;
-                for(int k = x-1; k>=0; k--){
-                    p++;
-                    if(!ct[k]){
-                        ct[arr[i]]--;
-                        arr[i] = k;
-                        ct[k] = 1;
-                        flag = true;
-                        ans+=p;
-                        break;
-                    }
+        for(char x = 'a'; x<='z'; x++){
+            int freq = arr[x];
+            for(char p = 'a'; freq && p<='z'; p++){
+                if(p!=x && freq && arr[p] == freq){
+                    ans++;
+                    freq--;
+                    arr[x]--;
+                    p = 'a';
+                    p--;
                 }
-                if(!flag){
-                  ans+=p;  
-                  ct[arr[i]]--;
-                } 
-                
             }
         }
         return ans;
