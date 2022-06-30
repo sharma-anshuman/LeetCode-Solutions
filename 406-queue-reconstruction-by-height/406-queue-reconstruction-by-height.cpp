78279370024fddec99bae5b9d7d2f4c019f522fa
Anshuman Sharma <code4anshu@gmail.com>
2022-06-30 11:54:02 +0530

    Time: 101 ms (76.71%), Space: 12.1 MB (49.77%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
-        int n = people.size();
+       int n = people.size();
         vector<vector<int>> ans(n, vector<int>(2, -1));
-        vector<pair<int, int>> pr(n);
+        vector<pair<int, int>> arr(n);
         for(int i = 0; i<n; i++){
-            pr[i] = {people[i][0], people[i][1]};
+            arr[i] = {people[i][0], people[i][1]};
         }
-        stable_sort(pr.begin(), pr.end());
+        stable_sort(arr.begin(), arr.end());
         
         for(int i = 0; i<n; i++){
-            int ct = pr[i].second;
+            int freq = arr[i].second;
             for(int j = 0; j<n; j++){
-                if(!ct && ans[j][0] == -1 && ans[j][1] == -1){
-                    ans[j][0] = pr[i].first;
-                    ans[j][1] = pr[i].second;
+                if(freq == 0 && ans[j][0] == -1){
+                    ans[j][0] = arr[i].first;
+                    ans[j][1] = arr[i].second;
                     break;
                 }
-                if(ans[j][0] == -1 && ans[j][1] == -1){
-                    ct--;
+                if((ans[j][0] == -1) || ans[j][0]>=arr[i].first){
+                    freq--;
                     continue;
-                }
-                if(ans[j][0]>=pr[i].first){
-                    ct--;
                 }
             }
         }
         return ans;
     }
 };
+
+
+    // 4 4     5 0     5 0     5 0     5 0    -1 -1
+    // 5 0     5 2     6 1     7 0     7 0    -1 -1
+    // 5 2     6 1     5 2     6 1     5 2    -1 -1
+    // 6 1     7 0     7 0     5 2     6 1    -1 -1
+    // 7 0     4 4     4 4     4 4     4 4    -1 -1
+    // 7 1     7 1     7 1     7 1     7 1    -1 -1
