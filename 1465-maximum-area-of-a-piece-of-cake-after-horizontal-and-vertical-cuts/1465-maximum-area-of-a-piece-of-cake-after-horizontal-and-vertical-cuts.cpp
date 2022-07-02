d52c6fe7cecc568a60adfc1157f65af04af52d3b
Anshuman Sharma <code4anshu@gmail.com>
2022-07-02 13:53:03 +0530

    Time: 104 ms (48.69%), Space: 32.3 MB (40.04%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -2,22 +2,30 @@ class Solution {
 public:
     int maxArea(int h, int w, vector<int>& hc, vector<int>& vc) {
         int nHc = hc.size(), nVc = vc.size();
+        //sorting horizontal and vertical cuts vector
         sort(vc.begin(), vc.end());
         sort(hc.begin(), hc.end());
+        
+        //assigning maximum hight as (first cut from starting boundary) hc[0] - 0
         int hMax = hc[0];
         for(int i = 1; i<nHc; i++){
             hMax = max(hMax, hc[i] - hc[i-1]);
         }
+        //taking the maximum of max height till now and the legnth from last cut to the end
         hMax = max(hMax, h-hc[nHc-1]);
         
+        //doing the same for vertical cuts
         int vMax = vc[0];
         for(int i = 1; i<nVc; i++){
             vMax = max(vMax, vc[i] - vc[i-1]);
         }
         vMax = max(vMax, w-vc[nVc-1]);
-        //cout<<hMax<<' '<<vMax<<endl;
         const int MOD = 1e9 + 7;
+        
+        //taking the multiplication answer in long long so as to avoid overflow
         long long int ans = (long long)(vMax) * (long long)(hMax);
+        
+        //returning ans%(1e9+7)
         return (int)(ans%MOD);
     }
 };
