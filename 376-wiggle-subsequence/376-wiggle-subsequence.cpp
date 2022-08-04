a3ec76de5e7b34746838473baeaae01bce7fe06b
Anshuman Sharma <code4anshu@gmail.com>
2022-08-05 01:26:52 +0530

    Time: 3 ms (63.38%), Space: 7.2 MB (21.18%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -9,24 +9,17 @@ public:
             else if(x<0) arr.emplace_back(0);
         }
         //for 1 first;
-        int one = 0, lst = 1, zero = 0;
+        int one = 0, lst = 1, zero = 0, lst1 = 0;
         for(int i = 0; i<arr.size(); i++){
             if(arr[i] == lst){
                 lst = !lst;
                 one++;
             }
-        }
-        lst = 0;
-        for(int i = 0; i<arr.size(); i++){
-            if(arr[i] == lst){
-                lst = !lst;
+            if(arr[i] == lst1){
+                lst1 = !lst1;
                 zero++;
             }
         }
-        
         return max(one, zero)+1;
-        
-        //0 1 0 0 1 1 0 1 0
-        
     }
 };
