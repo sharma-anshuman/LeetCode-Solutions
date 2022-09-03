94cb6938ad2c59e6ed017cfbff7b936c1232b2f4
Anshuman Sharma <code4anshu@gmail.com>
2022-09-03 22:57:30 +0530

    Time: 3 ms (74.35%), Space: 7.4 MB (75.91%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     vector<int> arr;
+    int toInt(string s){
+        int ans = 0, n = s.size();
+        for(int i = 0; i<n; i++){ ans*=10, ans+=(s[i] - '0');}
+        return ans;
+    }
     char st[10] = {'0','1','2','3','4','5','6','7','8','9'};
     void solve(string s, int n, int k){
         if(s.size() == n){
-            arr.emplace_back(stoi(s));
+            arr.emplace_back(toInt(s));
             return;
         }
         for(int i = 0; i<10; i++){
             int x = s[s.size() - 1] - '0';
-            if(abs(x-i) == k){
-                solve(s+st[i], n, k);
-            }
+            if(abs(x-i) == k) solve(s+st[i], n, k);
         }
     }
     vector<int> numsSameConsecDiff(int n, int k) {
         string xy[] = {"1","2","3","4","5","6","7","8","9"};
-        for(int i = 0; i<9; i++){
-            solve(xy[i], n, k);
-        }
+        for(int i = 0; i<9; i++) solve(xy[i], n, k);
         return arr;
     }
 };
